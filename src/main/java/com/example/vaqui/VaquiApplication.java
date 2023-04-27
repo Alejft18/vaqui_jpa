95bb98b657356ae3218a6d5fd27db2aa5f8fc7e6
Alejft18 <joseon85@gmail.com>
2023-04-27 10:34:22 -0500

    Initial commit

---
+++ src/main/java/com/example/vaqui/VaquiApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaquiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vaqui/VaquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
