fca4b97a600fdb6255261962c2f5300e27b80b05
yury1031 <yuryrueda2013@gmail.com>
2023-05-03 09:03:12 -0500

    ternero y toro

--- src/main/java/com/example/vaqui/Entidad/Toro.java
+++ src/main/java/com/example/vaqui/Entidad/Toro.java
@@ -1,4 +1,105 @@
 package com.example.vaqui.Entidad;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "tbl_toro")
 public class Toro {
+
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "id", referencedColumnName = "id", nullable = false)
+    @JsonIgnore
+    private General tbl_general;
+
+    @Column(nullable = false)
+    private Double peso_kilos;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date fecha_extraccion;
+
+    @Column(nullable = false)
+    private int vacas_montadas;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date fecha_Revision;
+    @Column(nullable = false, length = 20)
+    private String categoria;
+
+    public Toro(General tbl_general, Double peso_kilos, Date fecha_extraccion, int vacas_montadas, Date fecha_Revision, String categoria) {
+        this.tbl_general = tbl_general;
+        this.peso_kilos = peso_kilos;
+        this.fecha_extraccion = fecha_extraccion;
+        this.vacas_montadas = vacas_montadas;
+        this.fecha_Revision = fecha_Revision;
+        this.categoria = categoria;
+    }
+
+    public Toro() {
+    }
+
+    public General getTbl_general() {
+        return tbl_general;
+    }
+
+    public void setTbl_general(General tbl_general) {
+        this.tbl_general = tbl_general;
+    }
+
+    public Double getPeso_kilos() {
+        return peso_kilos;
+    }
+
+    public void setPeso_kilos(Double peso_kilos) {
+        this.peso_kilos = peso_kilos;
+    }
+
+    public Date getFecha_extraccion() {
+        return fecha_extraccion;
+    }
+
+    public void setFecha_extraccion(Date fecha_extraccion) {
+        this.fecha_extraccion = fecha_extraccion;
+    }
+
+    public int getVacas_montadas() {
+        return vacas_montadas;
+    }
+
+    public void setVacas_montadas(int vacas_montadas) {
+        this.vacas_montadas = vacas_montadas;
+    }
+
+    public Date getFecha_Revision() {
+        return fecha_Revision;
+    }
+
+    public void setFecha_Revision(Date fecha_Revision) {
+        this.fecha_Revision = fecha_Revision;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Toro{" +
+                "tbl_general=" + tbl_general +
+                ", peso_kilos=" + peso_kilos +
+                ", fecha_extraccion=" + fecha_extraccion +
+                ", vacas_montadas=" + vacas_montadas +
+                ", fecha_Revision=" + fecha_Revision +
+                ", categoria='" + categoria + '\'' +
+                '}';
+    }
 }
