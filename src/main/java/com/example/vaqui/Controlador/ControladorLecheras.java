7be1c691dee949a7b7a63dedfdcafe940b33d6ba
david <davidestebanmesa11@gmail.com>
2023-05-04 10:40:55 -0500

    Metodo lecheras

---
+++ src/main/java/com/example/vaqui/Controlador/ControladorLecheras.java
@@ -0,0 +1,29 @@
+package com.example.vaqui.Controlador;
+
+import com.example.vaqui.Entidad.Lechera;
+import com.example.vaqui.Servicios.ServiciosLecheras;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin(originPatterns = "*", maxAge = 3600)
+public class ControladorLecheras {
+
+    ServiciosLecheras servicios;
+
+    public ControladorLecheras(ServiciosLecheras servicios){this.servicios = servicios;}
+
+    @GetMapping("/listarLecheras")
+    public ArrayList<Lechera> listarLechera(){return servicios.listar();}
+
+    @PostMapping("/agregarLecheras")
+    public String agregarLecheras(@RequestBody Lechera lechera){return servicios.agregarLecheras(lechera);}
+
+    @DeleteMapping("/eliminarLecheras/{id}")
+    public String eliminarLecheras(@PathVariable("id")int id){return servicios.eliminarLecheras(id);}
+
+    @PutMapping("/actualizarLecheras")
+    public String actualizarLecheras(@RequestBody Lechera lechera){return servicios.actualizarLecheras(lechera);}
+
+}
---
+++ src/main/java/com/example/vaqui/Servicios/ServiciosLecheras.java
@@ -0,0 +1,11 @@
+package com.example.vaqui.Servicios;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiciosLecheras {
+
+
+
+
+}
