c1ff93d7b1d6e92f27f451510980206523358b8c
stiveng2605 <esga1526@gmail.com>
2023-05-11 11:30:30 -0500

    Controlador y servicio general

---
+++ src/main/java/com/example/vaqui/Controlador/ControladorGeneral.java
@@ -0,0 +1,29 @@
+package com.example.vaqui.Controlador;
+
+import com.example.vaqui.Entidad.General;
+import com.example.vaqui.Servicios.ServicioGeneral;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ControladorGeneral {
+
+    ServicioGeneral servicio;
+
+    public ControladorGeneral(ServicioGeneral servicio) {
+        this.servicio = servicio;
+    }
+
+    @GetMapping("/listarGeneral")
+    public ArrayList<General> listarGeneral(){
+        return servicio.listar();
+    }
+
+    @PostMapping("/agregarGeneral")
+    public String agregarGeneral(@RequestBody General general){
+        return servicio.agregarGeneral(general);
+    }
+
+}
---
+++ src/main/java/com/example/vaqui/Servicios/ServicioGeneral.java
@@ -0,0 +1,23 @@
+package com.example.vaqui.Servicios;
+
+import com.example.vaqui.Entidad.General;
+import com.example.vaqui.Repositorio.GeneralRepository;
+
+import java.util.ArrayList;
+
+public class ServicioGeneral {
+    GeneralRepository repository;
+
+    public ServicioGeneral(GeneralRepository repository) {
+        this.repository = repository;
+    }
+
+    public ArrayList<General> listar(){
+        return (ArrayList<General>) repository.findAll();
+    }
+
+    public  String agregarGeneral(General general){
+        repository.save(general);
+        return "Ingreso exitoso";
+    }
+}
