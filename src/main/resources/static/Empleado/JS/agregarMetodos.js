edd42dc239d742819d500317339eac0bbc578bbb
david <davidestebanmesa11@gmail.com>
2023-06-27 17:09:21 -0500

    Subiendo FRONT END

--- src/main/resources/static/Empleado/JS/agregarMetodos.js
+++ src/main/resources/static/Empleado/JS/agregarMetodos.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+    // Agregar BOVINO GENERAL
   $("#agregar").on('click',function(){
       let datos = {
           raza: $('#raza').val(),
@@ -27,4 +29,39 @@ $(document).ready(function(){
         }
      });
   });
+
+    // Agregar USUARIO
+        $("#registrar").on('click',function(){
+            let datos = {
+                id: parseInt($('#id').val()),
+                nombre: $('#nombre').val(),
+                apellido: $('#apellido').val(),
+                telefono: parseInt($('#telefono').val()),
+                correo: $('#correo').val(),
+                contrasena: $('#contraseña').val(),
+                rol: $('#rol').val(),
+                area: $('#area').val()
+                // imagen: $('#seleccionar__imagen').val(),
+            }
+
+            console.log(datos);
+            let datosEnvio = JSON.stringify(datos);
+            console.log(datosEnvio);
+
+            $.ajax({
+                url: "http://localhost:8080/agregarUsuario",
+                type: "POST",
+                data: datosEnvio,
+                contentType: "application/JSON",
+                dataType: "JSON",
+               success: function(response) {
+                  alert("El usuario se agregó exitosamente."); // Muestra un mensaje de éxito
+               },
+               error: function(xhr, status, error) {
+                  alert("Error al agregar el usuario: " + xhr.responseText); // Muestra un mensaje de error con el detalle del error
+               }
+           });
+        });
+
+
 });
