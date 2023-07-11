646541fcd11c51efe7d1eb398afeb78000635888
david <davidestebanmesa11@gmail.com>
2023-07-10 19:04:02 -0500

    Logica basica LOGIN

--- src/main/resources/static/Adminstrador/JS/actualizarEmpleado.js
+++ src/main/resources/static/Adminstrador/JS/actualizarEmpleado.js
@@ -14,7 +14,8 @@ $(document).ready(function(){
                     '<td>' + datos[i].correo + '</td>' +
                     '<td>' + datos[i].contrasena + '</td>' +
                     '<td>' + datos[i].rol + '</td>' +
-                    '<td>' + datos[i].area + '</td>');
+                    '<td>' + datos[i].area + '</td>' +
+                    '<td><button class="btn-actualizar" data-id="' + datos[i].id + '">Actualizar</button></td>');
         tbody.append(fila);
         }
     }
@@ -70,6 +71,74 @@ $(document).ready(function(){
               buscarPorId();
             }
           });
+
+
+
+        // Obtén los datos del formulario
+        let idElement = document.getElementById('id');
+        let nombreElement = document.getElementById('nombre');
+        let apellidoElement = document.getElementById('apellido');
+        let telefonoElement = document.getElementById('telefono');
+        let correoElement = document.getElementById('correo');
+        let contrasenaElement = document.getElementById('contraseña');
+        let rolElement = document.getElementById('rol');
+        let areaElement = document.getElementById('area');
+
+        // Verifica que los elementos del formulario existan y tengan un valor
+        if (idElement && nombreElement && apellidoElement && telefonoElement && correoElement && contrasenaElement && rolElement && areaElement) {
+          let id = idElement.value;
+          let nombre = nombreElement.value;
+          let apellido = apellidoElement.value;
+          let telefono = telefonoElement.value;
+          let correo = correoElement.value;
+          let contrasena = contrasenaElement.value;
+          let rol = rolElement.value;
+          let area = areaElement.value;
+
+          // Crea un objeto con los datos del usuario
+          let usuario = {
+            id: id,
+            nombre: nombre,
+            apellido: apellido,
+            telefono: telefono,
+            correo: correo,
+            contrasena: contrasena,
+            rol: rol,
+            area: area
+          };
+
+          // Realiza la llamada Ajax para actualizar el usuario
+          $.ajax({
+            url: 'http://localhost:8080/actualizarUsuario',
+            type: 'PUT',
+            data: JSON.stringify(usuario),
+            contentType: 'application/json',
+            success: function(response) {
+              // Maneja la respuesta del servidor
+              alert(response); // Muestra la respuesta en un cuadro de diálogo (puedes cambiar esto según tus necesidades)
+            },
+            error: function(error) {
+              // Maneja el error en caso de que ocurra
+              console.log(error); // Muestra el error en la consola (puedes cambiar esto según tus necesidades)
+            }
+          });
+        } else {
+          console.log('Error: No se encontraron elementos del formulario o faltan valores.'); // Muestra un mensaje de error en la consola
+        }
+
+
+        // Función para redirigir al formulario de actualizar con los datos del empleado seleccionado
+        function redireccionarActualizar(id) {
+          window.location.href = '../Gestion ingresar empleado.html' + id;
+        }
+
+        // Manejar el evento click del botón "Actualizar" en cada fila
+        $('#tabla tbody').on('click', 'tr', function() {
+          let id = $(this).find('td:first').text(); // Obtener el ID del empleado de la fila seleccionada
+          redireccionarActualizar(id); // Redireccionar al formulario de actualizar con el ID como parámetro
+        });
+
+
 
 });
 
