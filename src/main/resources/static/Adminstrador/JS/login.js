792adb51ed2c9f00cee7f70a5d44245fbd952c25
david <davidestebanmesa11@gmail.com>
2023-07-16 21:46:11 -0500

    Logica perfil sin funcionar

--- src/main/resources/static/Adminstrador/JS/login.js
+++ src/main/resources/static/Adminstrador/JS/login.js
@@ -21,57 +21,89 @@
 //    }
 //}
 
-    function validarFormulario(event) {
-        event.preventDefault(); // Prevenir el envío del formulario por defecto
+function validarFormulario(event) {
+    event.preventDefault();
 
-        // Obtener los valores del formulario
-        const id = document.getElementById('id').value;
-        const contraseña = document.getElementById('contraseña').value;
+    // Obtener los valores del formulario
+    const id = document.getElementById('id').value;
+    const contraseña = document.getElementById('contraseña').value;
 
-        // Validar que los campos no estén vacíos
-        if (id.trim() === '' || contraseña.trim() === '') {
-            alert('Por favor, ingrese el número de documento y la contraseña.');
-            return;
-        }
-
-        const url = `http://localhost:8080/buscarUsuario/${id}`;
-
-        fetch(url, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Usuario no encontrado');
-            }
-            return response.json();
-        })
-        .then(usuario => {
-            // Verificar si la contraseña coincide
-            if (usuario.contrasena === contraseña) {
-                // Verificar el rol del usuario
-                if (usuario.rol === 'empleada') {
-                    alert('Inicio de sesión exitoso como empleado');
-                    // Redireccionar a la página de empleado después del inicio de sesión
-                    window.location.href = '/Empleado/LandingPage.html';
-                } else if (usuario.rol === 'Administrador') {
-                    alert('Inicio de sesión exitoso como administrador');
-                    // Redireccionar a la página de administrador después del inicio de sesión
-                    window.location.href = '/Adminstrador/LandingPage.html';
-                } else {
-                    alert('Rol no reconocido');
-                }
-            } else {
-                alert('Documento y/o contraseña incorrectos');
-            }
-        })
-        .catch(error => {
-            alert('Documento y/o contraseña incorrectos');
-            console.error('Error:', error);
-        });
+    // Validar que los campos no estén vacíos
+    if (id.trim() === '' || contraseña.trim() === '') {
+        alert('Por favor, ingrese el número de documento y la contraseña.');
+        return;
     }
+
+    const url = `http://localhost:8080/buscarUsuario/${id}`;
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Usuario no encontrado');
+        }
+        return response.json();
+    })
+    .then(usuario => {
+        // Verificar si la contraseña coincide
+        if (usuario.contrasena === contraseña) {
+            // Verificar el rol del usuario
+            if (usuario.rol === 'empleada') {
+                alert('Inicio de sesión exitoso como empleado');
+                // Redireccionar a la página de empleado después del inicio de sesión
+                window.location.href = '/Empleado/LandingPage.html';
+            } else if (usuario.rol === 'Administrador') {
+                alert('Inicio de sesión exitoso como administrador');
+                // Redireccionar a la página de administrador después del inicio de sesión
+                window.location.href = '/Adminstrador/LandingPage.html';
+            } else {
+                alert('Rol no reconocido');
+            }
+
+            // Cargar los datos del usuario en el perfil
+            cargarPerfilUsuario(usuario.id); // Llama a la función para cargar los datos del usuario
+        } else {
+            alert('Documento y/o contraseña incorrectos');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+// Función para cargar los datos del perfil del usuario
+function cargarPerfilUsuario(id) {
+    const url = `http://localhost:8080/buscarUsuario/${id}`;
+    fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al obtener los datos del perfil');
+        }
+        return response.json();
+    })
+    .then(usuario => {
+        // Mostrar los datos del usuario en el perfil
+        document.getElementById('nombre').textContent = usuario.nombre;
+        document.getElementById('apellido').textContent = usuario.apellido;
+        document.getElementById('telefono').textContent = usuario.telefono;
+        document.getElementById('correo').textContent = usuario.correo;
+        document.getElementById('rol').textContent = usuario.rol;
+        document.getElementById('area').textContent = usuario.area;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
+
+
 
 
 
